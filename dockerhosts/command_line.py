ba7919c3b545a77c80c3342010b6cadf972fcce0
Gordon Yau <qunxyz@gmail.com>
2024-02-03 11:20:51 +0800

    fix no containers warning issue

--- dockerhosts/command_line.py
+++ dockerhosts/command_line.py
@@ -7,44 +7,46 @@ def refresh_hosts(client, hosts_file):
     containers = client.containers.list()
     docker_hosts = []
     docker_hosts_block = None
-    for container in containers:
-        status = container.status
-        if status != 'running': continue
+    if containers:
+        for container in containers:
+            status = container.status
+            if status != 'running': continue
 
-        name = container.name
-        networks = container.attrs['NetworkSettings']['Networks']
-        for network in networks:
-            ip = networks[network]['IPAddress']
-            if ip == '': continue
-            aliases = list(set(networks[network]['Aliases']))
-            for alias in aliases:
-                docker_hosts.append((name, alias, ip))
-                if not docker_hosts_block: docker_hosts_block = ''
-                docker_hosts_block += f"{ip} {alias}\n"
+            name = container.name
+            networks = container.attrs['NetworkSettings']['Networks']
+            for network in networks:
+                ip = networks[network]['IPAddress']
+                if ip == '': continue
+                aliases = list(set(networks[network]['Aliases']))
+                for alias in aliases:
+                    docker_hosts.append((name, alias, ip))
+                    if not docker_hosts_block: docker_hosts_block = ''
+                    docker_hosts_block += f"{ip} {alias}\n"
 
-    if docker_hosts_block:
-        others = None
-        with open(hosts_file, 'r') as f:
-            lines = f.readlines()
-            is_docker_host = False
-            for line in lines:
-                line = line.strip()
-                if not others: others = ''
-                if line == '':
-                    if not is_docker_host:
-                        others += f"{line}\n"
-                    continue
-                if line == DOCKER_START_TAG:
-                    is_docker_host = True
-                    continue
-                if line == DOCKER_END_TAG:
-                    is_docker_host = False
-                    continue
+    others = None
+    with open(hosts_file, 'r') as f:
+        lines = f.readlines()
+        is_docker_host = False
+        for line in lines:
+            line = line.strip()
+            if not others: others = ''
+            if line == '':
                 if not is_docker_host:
                     others += f"{line}\n"
-        if others:
-            with open(hosts_file, 'w+') as f:
-                f.write(others)
+                continue
+            if line == DOCKER_START_TAG:
+                is_docker_host = True
+                continue
+            if line == DOCKER_END_TAG:
+                is_docker_host = False
+                continue
+            if not is_docker_host:
+                others += f"{line}\n"
+    if others:
+        with open(hosts_file, 'w+') as f:
+            f.write(others)
+
+    if docker_hosts_block:
         with open(hosts_file, 'a+') as f:
             docker_hosts_block = f"{DOCKER_START_TAG}\n{docker_hosts_block}{DOCKER_END_TAG}\n"
             f.write(docker_hosts_block)
